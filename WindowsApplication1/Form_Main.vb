764525160dd17033a79da9d4a5c20b3aa5d26bde
Marc-Bender <marc.andre.bender@hotmail.com>
2021-01-08 20:17:50 +0100

    moved helper functions to own module

--- WindowsApplication1/Form_Main.vb
+++ WindowsApplication1/Form_Main.vb
@@ -1,15 +1,8 @@
-﻿Public Class Form_Main
-    ReadOnly timeout As Integer = 15 * 1000
+﻿Imports ClipboardHistory.md_ClipboardHistory_Helpers
+Public Class Form_Main
     Dim glob_Str_clipboardText As String
 
-    Private Declare Function RegisterHotKey Lib "user32" (ByVal hWnd As IntPtr, ByVal id As Integer, ByVal fsModifier As Integer, ByVal vk As Integer) As Integer
     Private Declare Sub UnregisterHotKey Lib "user32" (ByVal hWnd As IntPtr, ByVal id As Integer)
-
-    Private Const Lib_Const_Key_NONE As Integer = &H0
-    Private Const Lib_Const_Key_ALT As Integer = &H1
-    Private Const Lib_Const_Key_CTRL As Integer = &H2
-    Private Const Lib_Const_Key_SHIFT As Integer = &H4
-    Private Const Lib_Const_Key_WIN As Integer = &H8
 
     Private Const Lib_Const_WM_HOTKEY As Integer = &H312
 
@@ -17,203 +10,66 @@
         If m.Msg = Lib_Const_WM_HOTKEY Then
             Select Case m.WParam
                 Case 1 ' multiple hotkeys possible but only one needed yet
-                    helperFun_showSelector()
+                    Ifc_helperFun_showSelector()
             End Select
         End If
         MyBase.WndProc(m)
     End Sub
 
-    Private Sub helperFun_showSelector()
-        Me.Visible = True
-        Me.BringToFront()
-        Me.Activate()
-        Me.WindowState = FormWindowState.Normal
-    End Sub
-
-    Private Sub helperFun_clearClipboard()
-        Clipboard.Clear()
-        NotifyIcon1.BalloonTipText = My.Resources.Str_ClipboardEmptied_DE
-        NotifyIcon1.ShowBalloonTip(timeout)
-    End Sub
-
-    Private Sub helperFun_clearHistory()
-        ListBox_RecentClipboardEntries.Items.Clear()
-        NotifyIcon1.BalloonTipText = My.Resources.Str_HistoryEmptied_DE
-        NotifyIcon1.ShowBalloonTip(timeout)
-    End Sub
-
-    Private Sub helperFun_paste(textToPaste As String)
-        Clipboard.SetText(textToPaste)
-        My.Computer.Keyboard.SendKeys("%{TAB}")
-        Threading.Thread.Sleep(NumUD_DelayTime.Value) 'ms
-        My.Computer.Keyboard.SendKeys("+{INS}")
-    End Sub
-
     Private Sub HandlerFun_on_NotifyIcon1_MouseDown(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDown
         If e.Button = MouseButtons.Left Then
-            helperFun_showSelector()
+            Ifc_helperFun_showSelector()
         ElseIf e.Button = MouseButtons.Middle Then
-            helperFun_paste(ListBox_RecentClipboardEntries.Items.Item(0))
+            Ifc_helperFun_paste(ListBox_RecentClipboardEntries.Items.Item(0))
         Else
-            ' do nothing
+            ' do nothing; right click is handled via the context menu
         End If
     End Sub
 
     Private Sub HandlerFun_on_Form_Main_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
-        Me.CB_autoHide.Checked = My.Settings.b_autoHideOn
-        Me.CB_topMost.Checked = My.Settings.b_topMost
-        Me.TopMost = My.Settings.b_topMost
+        Ifc_helperFun_setCheckboxes()
+        Ifc_helperFun_setHotkey()
 
-        Me.CB_HotkeyModifiers_ALT.Checked = My.Settings.b_HotkeyModifier_ALT
-        Me.CB_HotkeyModifiers_CTRL.Checked = My.Settings.b_HotkeyModifier_CTRL
-        Me.CB_HotkeyModifiers_WIN.Checked = My.Settings.b_HotkeyModifier_WIN
-        Me.CB_HotkeyModifiers_SHIFT.Checked = My.Settings.b_HotkeyModifier_SHIFT
-        Me.ComB_Hotkey_Key.SelectedIndex = My.Settings.Uint_Hotkey_KeyIndex
-
-        HelperFun_SetHotkey()
-
-        If My.Settings.b_autoHideOn = True Then
-            Me.Visible = False
-            Me.MinimizeBox = False
-            Me.ControlBox = False
-        Else
-            Me.ControlBox = True
-            Me.MinimizeBox = True
-        End If
+        'Cb_autoHideOn.Checked is now set to the correct value so this works as well
+        Ifc_helperFun_handleAutoHideOnState()
 
         Me.NumUD_DelayTime.Value = My.Settings.Uint_DelayTime
         Me.Lbl_CheckForUpdatesLink.Text = My.Resources.Str_UpdateURL
         Me.Lbl_CurrentVersionNumber.Text = My.Resources.Str_VersionNumber
     End Sub
 
-    Private Sub HelperFun_SetHotkey()
-        Dim loc_Int_hotkeyModifier As Integer = 0
-        Dim loc_Uint_hotkeyKeyIndex As UInteger
-        Try
-            loc_Uint_hotkeyKeyIndex = ComB_Hotkey_Key.SelectedIndex
-        Catch ex As OverflowException
-            Return
-        End Try
-
-        Dim loc_RO_Array_hotkeyKeys As Keys() = {
-                                        Keys.A,
-                                        Keys.B,
-                                        Keys.C,
-                                        Keys.D,
-                                        Keys.E,
-                                        Keys.F,
-                                        Keys.G,
-                                        Keys.H,
-                                        Keys.I,
-                                        Keys.J,
-                                        Keys.K,
-                                        Keys.L,
-                                        Keys.M,
-                                        Keys.N,
-                                        Keys.O,
-                                        Keys.P,
-                                        Keys.Q,
-                                        Keys.R,
-                                        Keys.S,
-                                        Keys.T,
-                                        Keys.U,
-                                        Keys.V,
-                                        Keys.W,
-                                        Keys.X,
-                                        Keys.Y,
-                                        Keys.Z,
-                                        Keys.D1,
-                                        Keys.D2,
-                                        Keys.D3,
-                                        Keys.D4,
-                                        Keys.D5,
-                                        Keys.D6,
-                                        Keys.D7,
-                                        Keys.D8,
-                                        Keys.D9,
-                                        Keys.D0,
-                                        Keys.Delete,
-                                        Keys.Enter,
-                                        Keys.Back,
-                                        Keys.Space,
-                                        Keys.F1,
-                                        Keys.F2,
-                                        Keys.F3,
-                                        Keys.F4,
-                                        Keys.F5,
-                                        Keys.F6,
-                                        Keys.F7,
-                                        Keys.F8,
-                                        Keys.F9,
-                                        Keys.F10,
-                                        Keys.F11,
-                                        Keys.F12
-                                    }
-
-        If CB_HotkeyModifiers_CTRL.Checked = True Then
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or Lib_Const_Key_CTRL
-        Else
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier And Not Lib_Const_Key_CTRL
-        End If
-
-        If CB_HotkeyModifiers_ALT.Checked = True Then
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or Lib_Const_Key_ALT
-        Else
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier And Not Lib_Const_Key_ALT
-        End If
-
-        If CB_HotkeyModifiers_SHIFT.Checked = True Then
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or Lib_Const_Key_SHIFT
-        Else
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier And Not Lib_Const_Key_SHIFT
-        End If
-
-        If CB_HotkeyModifiers_WIN.Checked = True Then
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or Lib_Const_Key_WIN
-        Else
-            loc_Int_hotkeyModifier = loc_Int_hotkeyModifier And Not Lib_Const_Key_WIN
-        End If
-
-        My.Settings.b_HotkeyModifier_CTRL = CB_HotkeyModifiers_CTRL.Checked
-        My.Settings.b_HotkeyModifier_ALT = CB_HotkeyModifiers_ALT.Checked
-        My.Settings.b_HotkeyModifier_SHIFT = CB_HotkeyModifiers_SHIFT.Checked
-        My.Settings.b_HotkeyModifier_WIN = CB_HotkeyModifiers_WIN.Checked
-
-        RegisterHotKey(Me.Handle, 1, loc_Int_hotkeyModifier, loc_RO_Array_hotkeyKeys(loc_Uint_hotkeyKeyIndex))
-
-        My.Settings.Uint_Hotkey_KeyIndex = loc_Uint_hotkeyKeyIndex
-
-        My.Settings.Save()
-    End Sub
-
     Private Sub HandlerFun_on_Form_Main_FocusLost(sender As Object, e As EventArgs) Handles Me.Deactivate
         If My.Settings.b_autoHideOn = True Then
             Me.Visible = False
+        Else
+            'do nothing
         End If
     End Sub
+
     Private Sub HandlerFun_on_Form_Main_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles MyBase.FormClosing
         My.Settings.Save()
         UnregisterHotKey(Me.Handle, 1)
     End Sub
+
     Private Sub HandlerFun_on_TSMItem_End_Click(sender As Object, e As EventArgs) Handles TSMItem_end.Click
         NotifyIcon1.Visible = False
         End
     End Sub
 
     Private Sub HandlerFun_on_Btn_clearHistory_Click(sender As Object, e As EventArgs) Handles Btn_clearHistory.Click
-        helperFun_clearHistory()
+        Ifc_helperFun_clearClipboard()
     End Sub
 
     Private Sub HandlerFun_on_Btn_clearClipboard_Click(sender As Object, e As EventArgs) Handles Btn_clearClipboard.Click
-        helperFun_clearClipboard()
+        Ifc_helperFun_clearClipboard()
     End Sub
 
     Private Sub HandlerFun_on_TSMItem_clearHistory_Click(sender As Object, e As EventArgs) Handles TSMItem_clearHistory.Click
-        helperFun_clearHistory()
+        Ifc_helperFun_clearHistory()
     End Sub
 
     Private Sub HandlerFun_on_TSMItem_clearClipboard_Click(sender As Object, e As EventArgs) Handles TSMItem_clearClipboard.Click
-        helperFun_clearClipboard()
+        Ifc_helperFun_clearClipboard()
     End Sub
 
     Private Sub HandlerFun_on_Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
@@ -226,7 +82,6 @@
                         Exit For
                     End If
                 Next
-
             Catch ex As System.ArgumentNullException
             End Try
             ListBox_RecentClipboardEntries.Items.Insert(0, glob_Str_clipboardText)
@@ -244,7 +99,7 @@
         End If
         If e.Button = MouseButtons.Left Then
             Try
-                helperFun_paste(ListBox_RecentClipboardEntries.SelectedItem.ToString)
+                Ifc_helperFun_paste(ListBox_RecentClipboardEntries.SelectedItem.ToString)
             Catch ex As NullReferenceException
             End Try
         End If
@@ -258,12 +113,12 @@
             End Try
         End If
         If e.KeyCode = Keys.Enter Then
-            helperFun_paste(ListBox_RecentClipboardEntries.SelectedItem.ToString)
+            Ifc_helperFun_paste(ListBox_RecentClipboardEntries.SelectedItem.ToString)
         End If
     End Sub
 
     Private Sub HandlerFun_on_TSMItem_showSelector_Click(sender As Object, e As EventArgs) Handles TSMItem_showSelector.Click
-        helperFun_showSelector()
+        md_ClipboardHistory_Helpers.Ifc_helperFun_showSelector()
     End Sub
 
     Private Sub HandlerFun_on_CB_topMost_CheckedChanged(sender As Object, e As EventArgs) Handles CB_topMost.CheckedChanged
@@ -274,17 +129,7 @@
 
     Private Sub HandlerFun_on_CB_autoHide_CheckedChanged(sender As Object, e As EventArgs) Handles CB_autoHide.CheckedChanged
         My.Settings.b_autoHideOn = CB_autoHide.Checked
-        If CB_autoHide.Checked = True Then
-            My.Settings.b_topMost = False
-            Me.CB_topMost.Enabled = False
-            Me.CB_topMost.Checked = False
-            Me.MinimizeBox = False
-            Me.ControlBox = False
-        Else
-            Me.CB_topMost.Enabled = True
-            Me.MinimizeBox = True
-            Me.ControlBox = True
-        End If
+        Ifc_helperFun_handleAutoHideOnState()
         My.Settings.Save()
     End Sub
 
@@ -295,10 +140,10 @@
     End Sub
 
     Private Sub HandlerFun_on_Btn_ChangeHotkey_Click(sender As Object, e As EventArgs) Handles Btn_ChangeHotkey.Click
-        HelperFun_SetHotkey()
+        Ifc_helperFun_setHotkey()
     End Sub
 
-    Private Sub NumUD_DelayTime_ValueChanged(sender As Object, e As EventArgs) Handles NumUD_DelayTime.ValueChanged
+    Private Sub HandlerFun_NumUD_DelayTime_ValueChanged(sender As Object, e As EventArgs) Handles NumUD_DelayTime.ValueChanged
         My.Settings.Uint_DelayTime = NumUD_DelayTime.Value
         My.Settings.Save()
     End Sub
---
+++ WindowsApplication1/md_ClipboardHistory_Helpers.vb
@@ -0,0 +1,147 @@
+﻿Module md_ClipboardHistory_Helpers
+    Private ReadOnly RO_Uint_timeout As UInteger = 15 * 1000
+
+    Private Declare Function RegisterHotKey Lib "user32" (ByVal hWnd As IntPtr, ByVal id As Integer, ByVal fsModifier As Integer, ByVal vk As Integer) As Integer
+
+    Public Sub Ifc_helperFun_showSelector()
+        ClipboardHistory.Form_Main.Visible = True
+        ClipboardHistory.Form_Main.BringToFront()
+        ClipboardHistory.Form_Main.Activate()
+        ClipboardHistory.Form_Main.WindowState = FormWindowState.Normal
+    End Sub
+
+    Public Sub Ifc_helperFun_clearClipboard()
+        Clipboard.Clear()
+        ClipboardHistory.Form_Main.NotifyIcon1.BalloonTipText = My.Resources.Str_ClipboardEmptied_DE
+        ClipboardHistory.Form_Main.NotifyIcon1.ShowBalloonTip(RO_Uint_timeout)
+    End Sub
+
+    Public Sub Ifc_helperFun_clearHistory()
+        ClipboardHistory.Form_Main.ListBox_RecentClipboardEntries.Items.Clear()
+        ClipboardHistory.Form_Main.NotifyIcon1.BalloonTipText = My.Resources.Str_HistoryEmptied_DE
+        ClipboardHistory.Form_Main.NotifyIcon1.ShowBalloonTip(RO_Uint_timeout)
+    End Sub
+
+    Public Sub Ifc_helperFun_paste(ByVal arg_str_textToPaste As String)
+        Clipboard.SetText(arg_str_textToPaste)
+        My.Computer.Keyboard.SendKeys("%{TAB}")
+        Threading.Thread.Sleep(ClipboardHistory.Form_Main.NumUD_DelayTime.Value) 'ms
+        My.Computer.Keyboard.SendKeys("+{INS}")
+    End Sub
+
+    Public Sub Ifc_helperFun_setCheckboxes()
+        ClipboardHistory.Form_Main.CB_autoHide.Checked = My.Settings.b_autoHideOn
+        ClipboardHistory.Form_Main.CB_topMost.Checked = My.Settings.b_topMost
+        ClipboardHistory.Form_Main.TopMost = My.Settings.b_topMost
+
+        ClipboardHistory.Form_Main.CB_HotkeyModifiers_ALT.Checked = My.Settings.b_HotkeyModifier_ALT
+        ClipboardHistory.Form_Main.CB_HotkeyModifiers_CTRL.Checked = My.Settings.b_HotkeyModifier_CTRL
+        ClipboardHistory.Form_Main.CB_HotkeyModifiers_WIN.Checked = My.Settings.b_HotkeyModifier_WIN
+        ClipboardHistory.Form_Main.CB_HotkeyModifiers_SHIFT.Checked = My.Settings.b_HotkeyModifier_SHIFT
+        ClipboardHistory.Form_Main.ComB_Hotkey_Key.SelectedIndex = My.Settings.Uint_Hotkey_KeyIndex
+    End Sub
+
+    Public Sub Ifc_helperFun_handleAutoHideOnState()
+        If ClipboardHistory.Form_Main.CB_autoHide.Checked = True Then
+            My.Settings.b_topMost = False
+            ClipboardHistory.Form_Main.CB_topMost.Enabled = False
+            ClipboardHistory.Form_Main.CB_topMost.Checked = False
+            ClipboardHistory.Form_Main.MinimizeBox = False
+            ClipboardHistory.Form_Main.ControlBox = False
+        Else
+            ClipboardHistory.Form_Main.CB_topMost.Enabled = True
+            ClipboardHistory.Form_Main.MinimizeBox = True
+            ClipboardHistory.Form_Main.ControlBox = True
+        End If
+    End Sub
+
+    Public Sub Ifc_helperFun_setHotkey()
+        Dim loc_Int_hotkeyModifier As Integer = 0
+        Dim loc_Uint_hotkeyKeyIndex As UInteger
+
+        Try
+            loc_Uint_hotkeyKeyIndex = ClipboardHistory.Form_Main.ComB_Hotkey_Key.SelectedIndex
+        Catch ex As OverflowException
+            Return
+        End Try
+
+        Const Lib_Const_Key_ALT As Integer = &H1
+        Const Lib_Const_Key_CTRL As Integer = &H2
+        Const Lib_Const_Key_SHIFT As Integer = &H4
+        Const Lib_Const_Key_WIN As Integer = &H8
+
+        Dim loc_RO_Array_hotkeyKeys As Keys() = {
+                                        Keys.A,
+                                        Keys.B,
+                                        Keys.C,
+                                        Keys.D,
+                                        Keys.E,
+                                        Keys.F,
+                                        Keys.G,
+                                        Keys.H,
+                                        Keys.I,
+                                        Keys.J,
+                                        Keys.K,
+                                        Keys.L,
+                                        Keys.M,
+                                        Keys.N,
+                                        Keys.O,
+                                        Keys.P,
+                                        Keys.Q,
+                                        Keys.R,
+                                        Keys.S,
+                                        Keys.T,
+                                        Keys.U,
+                                        Keys.V,
+                                        Keys.W,
+                                        Keys.X,
+                                        Keys.Y,
+                                        Keys.Z,
+                                        Keys.D1,
+                                        Keys.D2,
+                                        Keys.D3,
+                                        Keys.D4,
+                                        Keys.D5,
+                                        Keys.D6,
+                                        Keys.D7,
+                                        Keys.D8,
+                                        Keys.D9,
+                                        Keys.D0,
+                                        Keys.Delete,
+                                        Keys.Enter,
+                                        Keys.Back,
+                                        Keys.Space,
+                                        Keys.F1,
+                                        Keys.F2,
+                                        Keys.F3,
+                                        Keys.F4,
+                                        Keys.F5,
+                                        Keys.F6,
+                                        Keys.F7,
+                                        Keys.F8,
+                                        Keys.F9,
+                                        Keys.F10,
+                                        Keys.F11,
+                                        Keys.F12
+                                    }
+
+        'starting out with loc_Int_hotkeyModifier = 0 so resetting unset bits is not neccessary
+        loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or (Lib_Const_Key_CTRL And ClipboardHistory.Form_Main.CB_HotkeyModifiers_CTRL.Checked)
+        loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or (Lib_Const_Key_ALT And ClipboardHistory.Form_Main.CB_HotkeyModifiers_ALT.Checked)
+        loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or (Lib_Const_Key_SHIFT And ClipboardHistory.Form_Main.CB_HotkeyModifiers_SHIFT.Checked)
+        loc_Int_hotkeyModifier = loc_Int_hotkeyModifier Or (Lib_Const_Key_WIN And ClipboardHistory.Form_Main.CB_HotkeyModifiers_WIN.Checked)
+
+        My.Settings.b_HotkeyModifier_CTRL = ClipboardHistory.Form_Main.CB_HotkeyModifiers_CTRL.Checked
+        My.Settings.b_HotkeyModifier_ALT = ClipboardHistory.Form_Main.CB_HotkeyModifiers_ALT.Checked
+        My.Settings.b_HotkeyModifier_SHIFT = ClipboardHistory.Form_Main.CB_HotkeyModifiers_SHIFT.Checked
+        My.Settings.b_HotkeyModifier_WIN = ClipboardHistory.Form_Main.CB_HotkeyModifiers_WIN.Checked
+
+        RegisterHotKey(ClipboardHistory.Form_Main.Handle, 1, loc_Int_hotkeyModifier, loc_RO_Array_hotkeyKeys(loc_Uint_hotkeyKeyIndex))
+
+        My.Settings.Uint_Hotkey_KeyIndex = loc_Uint_hotkeyKeyIndex
+
+        My.Settings.Save()
+
+    End Sub
+
+End Module
